750c0dfb35d58ff0fd35ccb397e32f971f0a29d0
Jimmy Wärting <jimmy@warting.se>
2022-01-18 18:20:29 +0100

    misc updates

---
+++ test/own-misc-test.js
@@ -0,0 +1,216 @@
+// @ts-nocheck
+// @ts-ignore
+
+import fs from 'node:fs'
+import buffer from 'node:buffer'
+import syncBlob, { blobFromSync, blobFrom, fileFromSync, fileFrom } from '../from.js'
+
+const license = fs.readFileSync('./LICENSE')
+
+test_blob(() => new Blob([
+  'a',
+  new Uint8Array([98]),
+  new Uint16Array([25699]),
+  new Uint8Array([101]).buffer,
+  Buffer.from('f'),
+  new Blob(['g']),
+  {},
+  new URLSearchParams('foo')
+]), {
+  desc: 'Blob ctor parts',
+  expected: 'abcdefg[object Object]foo=',
+  type: '',
+  length: 26
+})
+
+promise_test(async () => {
+  assert_equals(fileFromSync('./LICENSE').name, 'LICENSE')
+  assert_equals((await fileFrom('./LICENSE')).name, 'LICENSE')
+}, 'file from returns the same name')
+
+// Could not find similar test on WPT
+test(() => {
+  const now = new Date()
+  assert_equals(new File([], '', { lastModified: now }).lastModified, +now)
+  assert_equals(new File([], '', { lastModified: +now }).lastModified, +now)
+  assert_equals(new File([], '', { lastModified: 100 }).lastModified, 100)
+  assert_equals(new File([], '', { lastModified: '200' }).lastModified, 200)
+  assert_equals(new File([], '', { lastModified: true }).lastModified, 1)
+  assert_equals(new File([], '', { lastModified: false }).lastModified, 0)
+  assert_approx_equals(new File([], '').lastModified, Date.now(), 0.020)
+  assert_approx_equals(new File([], '', { lastModified: undefined }).lastModified, Date.now(), 0.020)
+}, 'File sets current time')
+
+// Could not find similar test on WPT
+test(() => {
+  const values = ['Not a Number', [], {}, null]
+  // I can't really see anything about this in the spec,
+  // but this is how browsers handle type casting for this option...
+  for (const lastModified of values) {
+    const file = new File([], '', { lastModified })
+    assert_equals(file.lastModified, 0)
+  }
+}, 'Interpretes NaN value in lastModified option as 0')
+
+test(() => {
+  assert_equals(blobFromSync, syncBlob)
+}, 'default export is named exported blobFromSync')
+
+promise_test(async () => {
+  const { Blob, default: def } = await import('../index.js')
+  assert_equals(Blob, def)
+}, 'Can use named import - as well as default')
+
+// This was necessary to avoid large ArrayBuffer clones (slice)
+promise_test(async t => {
+  const buf = new Uint8Array(65590)
+  const blob = new Blob([buf])
+  let i = 0
+  // eslint-disable-next-line no-unused-vars
+  for await (const chunk of blob.stream()) {
+    i++
+  }
+
+  assert_equals(i, 2)
+}, 'Large chunks are divided into smaller chunks')
+
+// Couldn't find a test for this on WPT
+promise_test(async () => {
+  const buf = new Uint8Array([97])
+  const blob = new Blob([buf])
+  const chunk = await blob.stream().getReader().read()
+  assert_equals(chunk.value[0], 97)
+  chunk.value[0] = 98
+  assert_equals(await blob.text(), 'a')
+}, 'Blobs are immutable')
+
+/**
+ * Deviation from WPT: it's important to keep boundary value
+ * so we don't lowercase the type
+ * @see https://github.com/w3c/FileAPI/issues/43
+ */
+test(() => {
+  const type = 'multipart/form-data; boundary=----WebKitFormBoundaryTKqdrVt01qOBltBd'
+  assert_equals(new Blob([], { type }).type, type)
+  assert_equals(new File([], '', { type }).type, type)
+}, 'Dose not lowercase the type')
+
+
+test( // Because we have symbol.hasInstance it's best to test it...
+  () => (assert_false(null instanceof Blob), assert_false(null instanceof File)),
+ 'Instanceof check returns false for nullish values'
+)
+
+test( // Because browser normally never tries things taken for granted
+  () => assert_equals(new Blob().toString(), '[object Blob]'),
+  'blob.toString() returns [object Blob]'
+)
+
+test( // Because browser normally never tries things taken for granted
+  () => assert_equals(new File([], '').toString(), '[object File]'),
+  'file.toString() returns [object File]'
+)
+
+// fetch-blob uniques is that it supports arbitrary blobs too
+test(() => {
+  class File {
+    stream () {}
+    get [Symbol.toStringTag] () { return 'File' }
+  }
+  assert_true(new File() instanceof Blob)
+}, 'Blob-ish class is an instance of Blob')
+
+// fetch-blob uniques is that it supports arbitrary blobs too
+if (buffer.Blob) {
+  test_blob(() => new Blob([new buffer.Blob(['blob part'])]), {
+    desc: 'Can wrap buffer.Blob to a fetch-blob',
+    expected: 'blob part',
+    type: '',
+  })
+}
+
+/**
+ * Test if Blob can be constructed with BOM and keep it when casted to string
+ * Test if blob.text() can correctly remove BOM - `buffer.toString()` is bad
+ */
+promise_test(async () => {
+  const text = '{"foo": "bar"}'
+  const blob = new Blob([`\uFEFF${text}`])
+  assert_equals(blob.size, 17)
+  assert_equals(await blob.text(), text)
+  const ab = await blob.slice(0, 3).arrayBuffer()
+  assert_equals_typed_array(new Uint8Array(ab), new Uint8Array([0xEF, 0xBB, 0xBF]))
+}, 'Can wrap buffer.Blob to a fetch-blob')
+
+// Here to make sure our `toIterator` is working as intended
+promise_test(async () => {
+  const stream = new Blob(['Some content']).stream()
+
+  // Cancel the stream before start reading, or this will throw an error
+  await stream.cancel()
+  const reader = stream.getReader()
+  const { done, value: chunk } = await reader.read()
+
+  assert_true(done)
+  assert_equals(chunk, undefined)
+}, 'Blob stream() can be cancelled')
+
+/******************************************************************************/
+/*                                                                            */
+/*                   Test Blobs backed up by the filesystem                   */
+/*                                                                            */
+/******************************************************************************/
+
+promise_test(async () => {
+  assert_equals(fileFromSync('./LICENSE', 'text/plain').type, 'text/plain')
+  assert_equals(fileFromSync('./LICENSE').type, '')
+
+  assert_equals(blobFromSync('./LICENSE', 'text/plain').type, 'text/plain')
+  assert_equals(blobFromSync('./LICENSE').type, '')
+
+  assert_equals((await fileFrom('./LICENSE', 'text/plain')).type, 'text/plain')
+  assert_equals((await fileFrom('./LICENSE')).type, '')
+
+  assert_equals((await blobFrom('./LICENSE', 'text/plain')).type, 'text/plain')
+  assert_equals((await blobFrom('./LICENSE')).type, '')
+}, 'from utilities sets correct type')
+
+promise_test(async () => {
+  assert_equals(await blobFromSync('./LICENSE').text(), license.toString())
+  assert_equals(await fileFromSync('./LICENSE').text(), license.toString())
+  assert_equals(await (await blobFrom('./LICENSE')).text(), license.toString())
+  assert_equals(await (await fileFrom('./LICENSE')).text(), license.toString())
+}, 'blob part backed up by filesystem can be read')
+
+promise_test(async () => {
+  assert_equals(await blobFromSync('./LICENSE').text(), license.toString())
+  assert_equals(await fileFromSync('./LICENSE').text(), license.toString())
+  assert_equals(await (await blobFrom('./LICENSE')).text(), license.toString())
+  assert_equals(await (await fileFrom('./LICENSE')).text(), license.toString())
+}, 'blob part backed up by filesystem slice correctly')
+
+test(async () => {
+  const blob = blobFromSync('./LICENSE')
+  await new Promise(resolve => setTimeout(resolve, 2000))
+  const now = new Date()
+  // Change modified time
+  fs.utimesSync('./LICENSE', now, now)
+  const error = await blob.text().then(assert_unreached, e => e)
+  assert_equals(error.constructor.name, 'DOMException')
+  assert_equals(error instanceof Error, true)
+  assert_equals(error.name, 'NotReadableError')
+
+  const file = fileFromSync('./LICENSE')
+  // Above test updates the last modified date to now
+  assert_equals(typeof file.lastModified, 'number')
+  // The lastModifiedDate is deprecated and removed from spec
+  assert_false('lastModifiedDate' in file)
+  assert_approx_equals(file.lastModified, +now, 1000)
+}, 'Reading after modified should fail')
+
+promise_test(async () => {
+  assert_equals(await blobFromSync('./LICENSE').slice(0, 0).text(), '')
+  assert_equals(await blobFromSync('./LICENSE').slice(0, 3).text(), license.slice(0, 3).toString())
+  assert_equals(await blobFromSync('./LICENSE').slice(4, 11).text(), license.slice(4, 11).toString())
+  assert_equals(await blobFromSync('./LICENSE').slice(-11).text(), license.slice(-11).toString())
+}, 'slicing blobs backed up by filesystem returns correct string')
--- test/test-wpt-in-node.js
+++ test/test-wpt-in-node.js
@@ -1,133 +1,143 @@
 // Don't want to use the FileReader, don't want to lowerCase the type either
 // import from 'https://wpt.live/resources/testharnessreport.js'
-import {File, Blob} from '../from.js'
+import { File, Blob } from '../from.js'
 
+let hasFailed
 globalThis.self = globalThis
 await import('https://wpt.live/resources/testharness.js')
 
-// Should probably be fixed... should be able to compare a Blob to a File
-delete Blob[Symbol.hasInstance]
-
 setup({
   explicit_timeout: true,
-  explicit_done: true,
-});
+  explicit_done: true
+})
 
-function test_blob(fn, expectations) {
-  var expected = expectations.expected,
-      type = expectations.type,
-      desc = expectations.desc;
+function test_blob (fn, expectations) {
+  const expected = expectations.expected
+  const type = expectations.type
+  const desc = expectations.desc
+  const length = expectations.length
 
-  var t = async_test(desc);
-  t.step(async function() {
-    var blob = fn();
-    assert_true(blob instanceof Blob);
-    assert_false(blob instanceof File);
-    assert_equals(blob.type.toLowerCase(), type);
-    assert_equals(blob.size, expected.length);
-    assert_equals(await blob.text(), expected);
-    t.done();
-  });
+  const t = async_test(desc)
+  t.step(async function () {
+    const blob = fn()
+    assert_true(blob instanceof Blob)
+    assert_false(blob instanceof File)
+    assert_equals(blob.type.toLowerCase(), type)
+    assert_equals(await blob.text(), expected)
+    t.done()
+  })
 }
 
-function test_blob_binary(fn, expectations) {
-  var expected = expectations.expected,
-      type = expectations.type,
-      desc = expectations.desc;
+function test_blob_binary (fn, expectations) {
+  const expected = expectations.expected
+  const type = expectations.type
+  const desc = expectations.desc
 
-  var t = async_test(desc);
-  t.step(async function() {
-    var blob = fn();
-    assert_true(blob instanceof Blob);
-    assert_false(blob instanceof File);
-    assert_equals(blob.type.toLowerCase(), type);
-    assert_equals(blob.size, expected.length);
-    const result = await blob.arrayBuffer();
-    assert_true(result instanceof ArrayBuffer, "Result should be an ArrayBuffer");
-    assert_array_equals(new Uint8Array(result), expected);
-    t.done();
-  });
+  const t = async_test(desc)
+  t.step(async function () {
+    const blob = fn()
+    assert_true(blob instanceof Blob)
+    assert_false(blob instanceof File)
+    assert_equals(blob.type.toLowerCase(), type)
+    const result = await blob.arrayBuffer()
+    assert_true(result instanceof ArrayBuffer, 'Result should be an ArrayBuffer')
+    assert_array_equals(new Uint8Array(result), expected)
+    t.done()
+  })
 }
 
 // Assert that two TypedArray objects have the same byte values
 globalThis.assert_equals_typed_array = (array1, array2) => {
   const [view1, view2] = [array1, array2].map((array) => {
     assert_true(array.buffer instanceof ArrayBuffer,
-      'Expect input ArrayBuffers to contain field `buffer`');
-    return new DataView(array.buffer, array.byteOffset, array.byteLength);
-  });
+      'Expect input ArrayBuffers to contain field `buffer`')
+    return new DataView(array.buffer, array.byteOffset, array.byteLength)
+  })
 
   assert_equals(view1.byteLength, view2.byteLength,
-    'Expect both arrays to be of the same byte length');
+    'Expect both arrays to be of the same byte length')
 
-  const byteLength = view1.byteLength;
+  const byteLength = view1.byteLength
 
   for (let i = 0; i < byteLength; ++i) {
     assert_equals(view1.getUint8(i), view2.getUint8(i),
-      `Expect byte at buffer position ${i} to be equal`);
+      `Expect byte at buffer position ${i} to be equal`)
   }
 }
 
-let hasFailed
+globalThis.add_result_callback((test, ...args) => {
+  if ([
+    'Blob with type "A"',
+    'Blob with type "TEXT/HTML"',
+    'Getters and value conversions should happen in order until an exception is thrown.',
+    'Using type in File constructor: TEXT/PLAIN',
+    'Using type in File constructor: text/plain;charset=UTF-8'
+  ].includes(test.name)) return
 
-globalThis.add_result_callback(test => {
-  const INDENT_SIZE = 2;
-  var reporter = {}
-  if (test.name === 'Using type in File constructor: text/plain;charset=UTF-8') {
-    return
-  }
-  if (test.name === 'Using type in File constructor: TEXT/PLAIN') {
-    return
-  }
+  const INDENT_SIZE = 2
+  const reporter = {}
 
-  reporter.startSuite = name => console.log(`\n  ${(name)}\n`);
+  reporter.startSuite = name => console.log(`\n  ${(name)}\n`)
 
-  reporter.pass = message => console.log((indent(("√ ") + message, INDENT_SIZE)));
+  reporter.pass = message => console.log((indent(('√ ') + message, INDENT_SIZE)))
 
-  reporter.fail = message => console.log((indent("\u00D7 " + message, INDENT_SIZE)));
+  reporter.fail = message => console.log((indent('\u00D7 ' + message, INDENT_SIZE)))
 
-  reporter.reportStack = stack => console.log((indent(stack, INDENT_SIZE * 2)));
+  reporter.reportStack = stack => console.log((indent(stack, INDENT_SIZE * 2)))
 
-  function indent(string, times) {
-    const prefix = " ".repeat(times);
-    return string.split("\n").map(l => prefix + l).join("\n");
+  function indent (string, times) {
+    const prefix = ' '.repeat(times)
+    return string.split('\n').map(l => prefix + l).join('\n')
   }
 
   if (test.status === 0) {
-    reporter.pass(test.name);
+    reporter.pass(test.name)
   } else if (test.status === 1) {
-    reporter.fail(`${test.name}\n`);
-    reporter.reportStack(`${test.message}\n${test.stack}`);
-    hasFailed = true;
+    reporter.fail(`${test.name}\n`)
+    reporter.reportStack(`${test.message}\n${test.stack}`)
+    hasFailed = true
   } else if (test.status === 2) {
-    reporter.fail(`${test.name} (timeout)\n`);
-    reporter.reportStack(`${test.message}\n${test.stack}`);
-    hasFailed = true;
+    reporter.fail(`${test.name} (timeout)\n`)
+    reporter.reportStack(`${test.message}\n${test.stack}`)
+    hasFailed = true
   } else if (test.status === 3) {
-    reporter.fail(`${test.name} (incomplete)\n`);
-    reporter.reportStack(`${test.message}\n${test.stack}`);
-    hasFailed = true;
+    reporter.fail(`${test.name} (incomplete)\n`)
+    reporter.reportStack(`${test.message}\n${test.stack}`)
+    hasFailed = true
   } else if (test.status === 4) {
-    reporter.fail(`${test.name} (precondition failed)\n`);
-    reporter.reportStack(`${test.message}\n${test.stack}`);
-    hasFailed = true;
+    reporter.fail(`${test.name} (precondition failed)\n`)
+    reporter.reportStack(`${test.message}\n${test.stack}`)
+    hasFailed = true
   } else {
-    reporter.fail(`unknown test status: ${test.status}`);
-    hasFailed = true;
+    reporter.fail(`unknown test status: ${test.status}`)
+    hasFailed = true
   }
-  hasFailed && process.exit(1);
 })
 
 globalThis.File = File
 globalThis.Blob = Blob
 globalThis.garbageCollect = () => {}
-globalThis.document = {body: '[object HTMLBodyElement]'}
-globalThis.test_blob = test_blob;
-globalThis.test_blob_binary = test_blob_binary;
+globalThis.test_blob = test_blob
+globalThis.test_blob_binary = test_blob_binary
+// Cuz WPT don't clean up after itself
+globalThis.MessageChannel = class extends MessageChannel {
+  constructor () {
+    super()
+    setTimeout(() => {
+      this.port1.close()
+      this.port2.close()
+      this.port1.onmessage = this.port2.onmessage = null
+    }, 100)
+  }
+}
 
-import("https://wpt.live/FileAPI/file/File-constructor.any.js")
-import("https://wpt.live/FileAPI/blob/Blob-array-buffer.any.js")
-import("https://wpt.live/FileAPI/blob/Blob-slice-overflow.any.js")
-import("https://wpt.live/FileAPI/blob/Blob-slice.any.js")
-import("https://wpt.live/FileAPI/blob/Blob-stream.any.js")
-import("https://wpt.live/FileAPI/blob/Blob-text.any.js")
+import('https://wpt.live/FileAPI/file/File-constructor.any.js')
+import('https://wpt.live/FileAPI/blob/Blob-constructor.any.js')
+import('https://wpt.live/FileAPI/blob/Blob-array-buffer.any.js')
+import('https://wpt.live/FileAPI/blob/Blob-slice-overflow.any.js')
+import('https://wpt.live/FileAPI/blob/Blob-slice.any.js')
+import('https://wpt.live/FileAPI/blob/Blob-stream.any.js')
+import('https://wpt.live/FileAPI/blob/Blob-text.any.js')
+import('./own-misc-test.js')
+
+hasFailed && process.exit(1)
