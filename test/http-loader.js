dc295883669fbc41d39d502ffbae36f1c12f580a
Jimmy Wärting <jimmy@warting.se>
2022-01-20 14:00:15 +0100

    fix: misc updates (#132)
    * misc updates
    
    * bring back c8 for test coverage
    
    * use new loader
    
    * don't use engines
    
    * simplify ci
    
    * fix double slice problem

--- test/http-loader.js
+++ test/http-loader.js
@@ -1,8 +1,21 @@
-// https-loader.mjs
-import { get } from 'https';
+import { Buffer } from 'node:buffer'
+import fs from 'node:fs'
+import { get } from 'node:https'
 
-export function resolve(specifier, context, defaultResolve) {
-  const { parentURL = null } = context;
+const fetch = url => new Promise(rs => get(url, rs))
+const cache = new URL('./.cache/', import.meta.url)
+
+/**
+ * @param {string} specifier
+ * @param {{
+ *   conditions: !Array<string>,
+ *   parentURL: !(string | undefined),
+ * }} context
+ * @param {Function} defaultResolve
+ * @returns {Promise<{ url: string }>}
+ */
+export async function resolve (specifier, context, defaultResolve) {
+  const { parentURL = null } = context
 
   // Normally Node.js would error on specifiers starting with 'https://', so
   // this hook intercepts them and converts them into absolute URLs to be
@@ -10,42 +23,43 @@ export function resolve(specifier, context, defaultResolve) {
   if (specifier.startsWith('https://')) {
     return {
       url: specifier
-    };
+    }
   } else if (parentURL && parentURL.startsWith('https://')) {
     return {
       url: new URL(specifier, parentURL).href
-    };
+    }
   }
 
   // Let Node.js handle all other specifiers.
-  return defaultResolve(specifier, context, defaultResolve);
+  return defaultResolve(specifier, context, defaultResolve)
 }
 
-export function getFormat(url, context, defaultGetFormat) {
-  // This loader assumes all network-provided JavaScript is ES module code.
-  if (url.startsWith('https://')) {
-    return {
-      format: 'module'
-    };
-  }
-
-  // Let Node.js handle all other URLs.
-  return defaultGetFormat(url, context, defaultGetFormat);
-}
-
-export function getSource(url, context, defaultGetSource) {
+export async function load (url, context, defaultLoad) {
   // For JavaScript to be loaded over the network, we need to fetch and
   // return it.
   if (url.startsWith('https://')) {
-    return new Promise((resolve, reject) => {
-      let data = ''
-      get(url, async res => {
-        for await (const chunk of res) data += chunk;
-        resolve({ source: data });
-      }).on('error', (err) => reject(err));
-    });
+    const uuid = Buffer.from(url).toString('hex')
+    const cachedFile = new URL(uuid, cache)
+    let data = ''
+
+    // cache remote files for 1h
+    if (fs.existsSync(cachedFile) && fs.statSync(cachedFile).mtimeMs > Date.now() - 1000 * 60 * 60) {
+      data = fs.readFileSync(cachedFile, 'utf8')
+    } else {
+      const res = await fetch(url).catch(err => err)
+      for await (const chunk of res) data += chunk
+      fs.mkdirSync(cache, { recursive: true })
+      fs.writeFileSync(cachedFile, data)
+    }
+
+    return {
+      // This example assumes all network-provided JavaScript is ES module
+      // code.
+      format: 'module',
+      source: data
+    }
   }
 
   // Let Node.js handle all other URLs.
-  return defaultGetSource(url, context, defaultGetSource);
+  return defaultLoad(url, context, defaultLoad)
 }
